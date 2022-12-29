ecd52427078d2a2a8d3594a2670419d64b3d3c55
CodeSevenic <sevenicbookings@gmail.com>
2022-12-29 08:01:59 +0200

    PurpleWeb theme code update

--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@
  * 
  * @package purpleWeb
  */
+
+function purpleweb_enqueue_scripts() {
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(),[], filemtime
+    (get_template_directory() . '/style.css'), 'all');
+}
+
+add_action('wp_enqueue_scripts', 'purpleweb_enqueue_scripts');
