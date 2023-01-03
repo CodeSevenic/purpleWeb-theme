e4f62567b47c67500d6a3f9e41405eebe9a4be3e
CodeSevenic <sevenicbookings@gmail.com>
2023-01-03 08:15:37 +0200

    PurpleWeb theme code update

--- inc/classes/class-purpleweb-theme.php
+++ inc/classes/class-purpleweb-theme.php
@@ -1,1 +1,14 @@
 <?php
+/**
+ * Bootstraps the Theme
+ *
+ * @package PurpleWeb
+ */
+
+namespace PURPLEWEB_THEME\Inc;
+
+use PURPLEWEB\Inc\Traits\Singleton;
+
+class PURPLEWEB_THEME {
+    use Singleton;
+}
