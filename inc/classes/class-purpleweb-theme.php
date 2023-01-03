ca0fe7c43debdd91e7a4a64c52b7582a5c74197f
CodeSevenic <sevenicbookings@gmail.com>
2023-01-03 16:25:10 +0200

    PurpleWeb theme code update

--- inc/classes/class-purpleweb-theme.php
+++ inc/classes/class-purpleweb-theme.php
@@ -20,15 +20,34 @@ class PURPLEWEB_THEME {
 
     protected function setup_hooks() {
         // action and filters
-        add_action('wp_enqueue_scripts', 'purpleweb_enqueue_scripts');
+        add_action('wp_enqueue_scripts', [$this, 'register_styles']);
+        add_action('wp_enqueue_scripts', [$this, 'register_scripts']);
 
     }
 
     public function register_styles() {
+        // Register styles
+        wp_register_style('style-css', get_stylesheet_uri(),[], filemtime
+        (get_template_directory() . '/style.css'), 'all');
+
+        wp_register_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css',[],false, 'all');
+        // Enqueue styles
+        wp_enqueue_style('style-css');
+        wp_enqueue_style('bootstrap-css');
 
     }
 
     public function register_scripts() {
+        // Register scripts
+        wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js',
+            [],
+            filemtime
+            (get_template_directory() . '/assets/main.js'), true);
 
+        wp_register_script('bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.bundle.min.js',
+            ['jquery'],false, true);
+        // Enqueue scripts
+        wp_enqueue_script('main-js');
+        wp_enqueue_script('bootstrap-js');
     }
 }
