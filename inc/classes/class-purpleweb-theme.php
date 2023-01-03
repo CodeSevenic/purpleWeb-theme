a03f8048cd80a0ee57dfcd97aa56907e53160f19
CodeSevenic <sevenicbookings@gmail.com>
2023-01-03 08:36:12 +0200

    PurpleWeb theme code update

--- inc/classes/class-purpleweb-theme.php
+++ inc/classes/class-purpleweb-theme.php
@@ -11,4 +11,14 @@ use PURPLEWEB\Inc\Traits\Singleton;
 
 class PURPLEWEB_THEME {
     use Singleton;
+
+    protected function __construct()
+    {
+        // load class.
+        $this->set_hooks();
+    }
+
+    protected function set_hooks() {
+        // action and filters
+    }
 }
