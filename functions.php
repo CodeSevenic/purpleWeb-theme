ad01a8125a35f461ce59b576e407ccd0daf6667b
CodeSevenic <sevenicbookings@gmail.com>
2023-01-03 16:31:13 +0200

    PurpleWeb theme code update

--- functions.php
+++ functions.php
@@ -10,6 +10,10 @@ if(!defined('PURPLEWEB_DIR_PATH')) {
     define('PURPLEWEB_DIR_PATH', untrailingslashit(get_template_directory()));
 }
 
+if(!defined('PURPLEWEB_DIR_URI')) {
+    define('PURPLEWEB_DIR_URI', untrailingslashit(get_template_directory_uri()));
+}
+
 require_once PURPLEWEB_DIR_PATH . '/inc/helpers/autoloader.php';
 
 function purpleweb_get_theme_instance() {
