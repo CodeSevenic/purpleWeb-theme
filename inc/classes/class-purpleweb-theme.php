1f21bc2f279740e9198be785e45d00091cb987a5
CodeSevenic <sevenicbookings@gmail.com>
2023-02-17 13:16:25 +0200

    PurpleWeb code update

--- inc/classes/class-purpleweb-theme.php
+++ inc/classes/class-purpleweb-theme.php
@@ -9,47 +9,69 @@ namespace PURPLEWEB_THEME\Inc;
 
 use PURPLEWEB_THEME\Inc\Traits\Singleton;
 
-class PURPLEWEB_THEME {
+class PURPLEWEB_THEME
+{
 
     use Singleton;
 
-    protected function __construct() {
+    protected function __construct()
+    {
         // load class.
+        Assets::get_instance();
+
         $this->setup_hooks();
     }
 
-    protected function setup_hooks() {
+    protected function setup_hooks()
+    {
         /**
          * Actions.
          */
-        add_action('wp_enqueue_scripts', [$this, 'register_styles']);
-        add_action('wp_enqueue_scripts', [$this, 'register_scripts']);
-
+        add_action('after_setup_theme', [$this, 'setup_theme']);
     }
 
-    public function register_styles() {
-        // Register styles
-        wp_register_style('style-css', get_stylesheet_uri(),[], filemtime
-        (PURPLEWEB_DIR_PATH . '/style.css'), 'all');
+    public function setup_theme(): void
+    {
+        add_theme_support('title-tag');
+        add_theme_support('custom-logo', [
+            'header-text' => ['site-title', 'site-description'],
+            'height' => 100,
+            'width' => 400,
+            'flex-height' => true,
+            'flex-width' => true
+        ]);
+        add_theme_support('custom-background', [
+            'default-color' => '#fff',
+            'default-image' => '',
+            'default-repeat' => 'no-repeat'
+        ]);
 
-        wp_register_style('bootstrap-css', PURPLEWEB_DIR_URI . '/assets/src/library/css/bootstrap.min.css',[],false, 'all');
-        // Enqueue styles
-        wp_enqueue_style('style-css');
-        wp_enqueue_style('bootstrap-css');
+        add_theme_support('post-thumbnails');
 
+        add_theme_support('customize-selective-refresh-widgets');
+
+        add_theme_support('automatic-feed-links');
+
+        add_theme_support(
+            'html5',
+            [
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style'
+            ]
+        );
+        add_editor_style();
+        add_theme_support('wp-block-styles');
+        add_theme_support('align-wide');
+
+        global $content_width;
+        if (!isset($content_width)) {
+            $content_width = 1240;
+        }
     }
 
-    public function register_scripts() {
-        // Register scripts
-        wp_register_script('main-js', PURPLEWEB_DIR_URI . '/assets/main.js',
-            [],
-            filemtime
-            (PURPLEWEB_DIR_PATH . '/assets/main.js'), true);
-
-        wp_register_script('bootstrap-js', PURPLEWEB_DIR_URI . '/assets/src/library/js/bootstrap.bundle.min.js',
-            ['jquery'],false, true);
-        // Enqueue scripts
-        wp_enqueue_script('main-js');
-        wp_enqueue_script('bootstrap-js');
-    }
 }
