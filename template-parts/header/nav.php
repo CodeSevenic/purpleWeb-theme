a53dabeed919e07e0719fde409497e0cb3bd2d9f
CodeSevenic <sevenicbookings@gmail.com>
2023-03-25 11:01:22 +0200

    PurpleWeb code update

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -2,7 +2,7 @@
 /** *
  *Header Navigation template.
  *
- *@package purpleWeb
+ * @package purpleWeb
  */
 $menu_class = \PURPLEWEB_THEME\Inc\Menus::get_instance();
 $header_menu_id = $menu_class->get_menu_id('purpleweb-header-menu');
@@ -16,45 +16,44 @@ $header_menus = wp_get_nav_menu_items($header_menu_id);
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <?php
-        if ( function_exists('the_custom_logo')) {
-            the_custom_logo();
-        }
+    if (function_exists('the_custom_logo')) {
+        the_custom_logo();
+    }
     ?>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <?php
-        if(!empty($header_menus) && is_array($header_menus)) {?>
+        if (!empty($header_menus) && is_array($header_menus)) { ?>
             <ul class="navbar-nav mr-auto">
                 <?php
-                    foreach ($header_menus as $menu_item) {
-                        if(!$menu_item->menu_item_parent) {
-                            $child_menu_items = $menu_class->get_child_menu_items($header_menus, $menu_item->ID);
-                            $has_children = ! empty($child_menu_items) && is_array($child_menu_items);
-                            if(!$has_children) {?>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Link</a>
-                                </li>
-                                    <?php } else { ?>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Dropdown
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="#">Action</a>
-                                    </div>
-                                </li>
-                            <?php }
-                            ?>
-
-
-                       <?php }
+                foreach ($header_menus as $menu_item) {
+                    if (!$menu_item->menu_item_parent) {
+                        $child_menu_items = $menu_class->get_child_menu_items($header_menus, $menu_item->ID);
+                        $has_children = !empty($child_menu_items) && is_array($child_menu_items);
+                        if (!$has_children) { ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                        <?php } else { ?>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                </div>
+                            </li>
+                        <?php }
                     }
+                }
                 ?>
             </ul>
-       <?php }
+        <?php }
         ?>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
