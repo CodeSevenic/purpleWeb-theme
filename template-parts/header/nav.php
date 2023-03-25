a103485107a2b3b2a7d7f4c3b1b1e919f80ce0e5
CodeSevenic <sevenicbookings@gmail.com>
2023-03-25 11:09:50 +0200

    PurpleWeb code update

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -8,10 +8,6 @@ $menu_class = \PURPLEWEB_THEME\Inc\Menus::get_instance();
 $header_menu_id = $menu_class->get_menu_id('purpleweb-header-menu');
 
 $header_menus = wp_get_nav_menu_items($header_menu_id);
-
-//echo '<pre>';
-//print_r($header_menus);
-//wp_die();
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
