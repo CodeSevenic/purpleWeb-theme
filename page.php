b11d608cc0daa43afc540ceda4881dc77b557fd7
CodeSevenic <sevenicbookings@gmail.com>
2023-03-28 21:34:20 +0200

    PurpleWeb code update

---
+++ page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  Page template
+ *
+ * @package purbleWeb
+ */
+
+get_header();
+?>
+
+
+<div>Page Template</div>
+
+<?php get_footer(); ?>
---
+++ single-post.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *  Post template
+ *
+ * @package purbleWeb
+ */
+
+get_header();
+?>
+
+
+<div>Single Post Template
+</div>
+
+<?php get_footer(); ?>
