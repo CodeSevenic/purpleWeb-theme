7e789ba3aa35b2b5a9b8e4b7b980751c445be5ab
CodeSevenic <sevenicbookings@gmail.com>
2023-03-29 14:06:12 +0200

    PurpleWeb code update

--- index.php
+++ index.php
@@ -7,35 +7,45 @@
 get_header();
 ?>
 
-<div id="primary">
-    <main id="main" class="site-main mt-5">
-        <?php
-        if (have_posts()) { ?>
-            <div class="container">
-                <?php
-                if (is_home() && ! is_front_page()) { ?>
-                    <header class="mb-5">
-                        <h1 class="page-title screen-reader-text">
-                            <?php single_post_title(); ?>
-                        </h1>
-                    </header>
-              <?php  }
-                ?>
-                <div class="row">
+    <div id="primary">
+        <main id="main" class="site-main mt-5">
+            <?php
+            if (have_posts()) { ?>
+                <div class="container">
                     <?php
-                    $index = 0;
-                    $no_of_columns = 3;
-                    while (have_posts()) : the_post();
-                        the_title();
-                        the_excerpt();
-                    endwhile;
+                    if (is_home() && !is_front_page()) { ?>
+                        <header class="mb-5">
+                            <h1 class="page-title screen-reader-text">
+                                <?php single_post_title(); ?>
+                            </h1>
+                        </header>
+                    <?php }
                     ?>
+                    <div class="row">
+                        <?php
+                        $index = 0;
+                        $no_of_columns = 3;
+                        // Start the loop
+                        while (have_posts()) : the_post();
+                            if (0 === $index % $no_of_columns) { ?>
+                                <div class="col-lg-4 col-md-6 col-sm-12">
+
+
+                            <?php }
+
+                            $index++;
+
+                            if (0 !== $index && 0 === $index % $no_of_columns) { ?>
+                                </div>
+                            <?php }
+                        endwhile;
+                        ?>
+                    </div>
                 </div>
-            </div>
-       <?php }
-        ?>
-    </main>
-</div>
+            <?php }
+            ?>
+        </main>
+    </div>
 
 <?php
 get_footer();
