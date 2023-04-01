56428e7a961e3a7e6c5ee7b666e9556205f82430
CodeSevenic <sevenicbookings@gmail.com>
2023-04-01 20:48:19 +0200

    PurpleWeb code update

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -6,4 +6,10 @@ function get_the_post_custom_thumbnail($post_id, $size = 'featured-image', $addi
     if (null === $post_id) {
         $post_id = get_the_ID();
     }
+
+    if (has_post_thumbnail($post_id)) {
+        $default_attributes = [
+                'loading' => 'lazy'
+        ];
+    }
 }
