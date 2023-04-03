5a883fffe1b07bd449220e5124cf8102639c5ec7
CodeSevenic <sevenicbookings@gmail.com>
2023-04-03 11:57:42 +0200

    PurpleWeb code update

---
+++ inc/classes/class-meta-boxes.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Register Meta Boxes
+ *
+ * @package purpleWeb
+ */
+
+namespace PURPLEWEB_THEME\Inc;
+
+use PURPLEWEB_THEME\Inc\Traits\Singleton;
+
+class Meta_Boxes
+{
+    use Singleton;
+
+    protected function __construct()
+    {
+        // load class.
+        $this->setup_hooks();
+    }
+
+    protected function setup_hooks()
+    {
+        /**
+         * Actions.
+         */
+        add_action('add_meta_boxes', [$this, 'add_custom_meta_box']);
+    }
+
+    public function add_custom_meta_box(): void
+    {
+        $screens = ['post'];
+        foreach ($screens as $screen) {
+            add_meta_box(
+                'hide-page-title', // Unique ID
+                __('Hide page title', 'purbleweb'), // Box title
+                [$this, 'custom_meta_box_html'], // Content callback, must be type callback
+                $screen
+            );
+        }
+    }
+
+    public function custom_meta_box_html($post){
+        $value = get_post_meta($post->ID, '_hide_page_title', true);
+        ?>
+        <label for="purbleweb-field"><?php esc_html_e('Hide the page title', 'purpleweb'); ?></label>
+        <select name="purbleweb_field" id="purbleweb-field" class="postbox">
+            <option value=""><?php esc_html_e('Please select', 'purpleweb'); ?></option>
+            <option value="yes" <?php selected($value, 'yes'); ?>>Yes</option>
+            <option value="no" <?php selected($value, 'no'); ?>>No</option>
+        </select>
+<?php
+    }
+}
