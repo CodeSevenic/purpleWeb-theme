c8ed82cdbf983fe98f83153172bc030d00697b3a
CodeSevenic <sevenicbookings@gmail.com>
2023-04-08 09:31:55 +0200

    PurpleWeb code update

--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -50,7 +50,7 @@ class Meta_Boxes
         /**
          * Use nonce for verification
          */
-        wp_nonce_field( plugin_basename(__FILE__), 'hide_title_meta_box_nonce_name' );
+        wp_nonce_field(plugin_basename(__FILE__), 'hide_title_meta_box_nonce_name');
         ?>
         <label for="purpleweb-field"><?php esc_html_e('Hide the page title', 'purpleweb'); ?></label>
         <select name="purpleweb_hide_title_field" id="purpleweb-field" class="postbox">
@@ -71,16 +71,19 @@ class Meta_Boxes
          * When the post is saved or updated we get $_POST available
          * Check if the current user is authorized
          */
-        if (! current_user_can('edit_post', $post_id)) {
+        if (!current_user_can('edit_post', $post_id)) {
             return;
         }
 
         /**
          * Check if the nonce value we received is the same we created.
          */
-        if (! isset())
+        if (!isset($_POST['hide_title_meta_box_nonce_name']) ||
+            !wp_verify_nonce(plugin_basename(__FILE__), $_POST['hide_title_meta_box_nonce_name'])) {
+            return;
+        }
 
-        if (array_key_exists('purpleweb_hide_title_field',$_POST)) {
+        if (array_key_exists('purpleweb_hide_title_field', $_POST)) {
             update_post_meta(
                 $post_id,
                 '_hide_page_title',
