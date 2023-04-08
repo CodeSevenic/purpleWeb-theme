384c7684c1410d5b4adc0f8635968af005e370de
CodeSevenic <sevenicbookings@gmail.com>
2023-04-08 16:51:09 +0200

    PurpleWeb code update

--- template-parts/components/blog/entry-footer.php
+++ template-parts/components/blog/entry-footer.php
@@ -1,1 +1,15 @@
 <?php
+/**
+ * Template for entry footer.
+ *
+ * to be used inside of WordPress The Loop.
+ *
+ * @package purpleWeb
+ */
+?>
+
+<div class="entry-footer mt-4">
+    <?php
+
+    ?>
+</div>
