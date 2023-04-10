f0272bd2f4b61eba671fb4400340eaf608aae41d
CodeSevenic <sevenicbookings@gmail.com>
2023-04-10 10:10:45 +0200

    PurpleWeb code update

--- inc/classes/class-assets.php
+++ inc/classes/class-assets.php
@@ -33,10 +33,14 @@ class Assets {
         // Register styles
         wp_register_style('style-css', get_stylesheet_uri(), [], filemtime
         (PURPLEWEB_DIR_PATH . '/style.css'), 'all');
-
+        wp_register_script('main-js', PURPLEWEB_BUILD_JS_URI . '/main.js',
+            [],
+            filemtime
+            (PURPLEWEB_BUILD_JS_DIR_PATH . '/main.js'), true);
         wp_register_style('bootstrap-css', PURPLEWEB_DIR_URI . '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
-        wp_enqueue_style('fonts-css', get_template_directory_uri() . '/assets/src/library/fonts/fonts.css', [], false, 'all');
+
         // Enqueue styles
+        wp_enqueue_style('fonts-css', get_template_directory_uri() . '/assets/src/library/fonts/fonts.css', [], false, 'all');
         wp_enqueue_style('style-css');
         wp_enqueue_style('bootstrap-css');
 
