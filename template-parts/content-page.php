0e545532e8dc777ab24760dcd7d0fa4774075f25
CodeSevenic <sevenicbookings@gmail.com>
2023-06-04 13:41:42 +0200

    PurpleWeb Code Update 2023

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,8 +11,15 @@
     if (! is_home()) { ?>
         <header class="entry-header">
             <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-        </header>
+        </header> <!-- .entry-header -->
   <?php  }
     ?>
+
+    <div class="entry-content">
+        <?php
+        the_content();
+        
+        ?>
+    </div>
 
 </article>
