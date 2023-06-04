0746b547cff7eb89d3f33dcedf8451c7141a5433
CodeSevenic <sevenicbookings@gmail.com>
2023-06-04 13:46:40 +0200

    PurpleWeb Code Update 2023

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -6,19 +6,19 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class();?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <?php
-    if (! is_home()) { ?>
+    if (!is_home()) { ?>
         <header class="entry-header">
             <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
         </header> <!-- .entry-header -->
-  <?php  }
+    <?php }
     ?>
 
     <div class="entry-content">
         <?php
         the_content();
-        if(! is_home()) {
+        if (!is_home()) {
             wp_link_pages(
                 [
                     'before' => '<div class="page-links">' . esc_html__('Pages:', 'purpleWeb'),
@@ -29,8 +29,8 @@
         ?>
     </div><!-- .entry-content -->
 
-    <footer class="">
-
-    </footer>
+    <footer class="entry-footer">
+        <?php edit_post_link(esc_html__('Edit', 'purpleWeb'), '<span class="edit-link">', '</span>'); ?>
+    </footer><!-- .entry-footer -->
 
 </article>
